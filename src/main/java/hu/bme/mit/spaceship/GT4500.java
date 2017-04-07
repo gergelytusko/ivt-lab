03577e40695bad4604ed7d5f27eb9d4b64cbe208
hey <gergely.tusko@gmail.com>
2017-04-07 04:15:15 -0700

    minor changes

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,8 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+
+      if(firingMode == SINGLE){
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,9 +74,8 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
-
-      case ALL:
+      }
+      else if(firingMode == ALL){
         // try to fire both of the torpedos
         //TODO implement feature
         if (primaryTorpedoStore.isEmpty() || secondaryTorpedoStore.isEmpty()) {
@@ -86,9 +85,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = secondaryTorpedoStore.fire(1);
 
         }
-
-        break;
-    }
+      }
 
     return firingSuccess;
   }
